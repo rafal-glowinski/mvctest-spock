61f9b68d017629d1c4523ae525f7337b80c46826
Rafal Glowinski <rafal.glowinski@allegro.pl>
2018-03-17 14:33:09 +0100

    Initial commit with generated project

---
+++ src/main/kotlin/com/rg/webmvctest/WebmvctestApplication.kt
@@ -0,0 +1,11 @@
+package com.rg.webmvctest
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class WebmvctestApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(WebmvctestApplication::class.java, *args)
+}
---
+++ src/test/kotlin/com/rg/webmvctest/WebmvctestApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.rg.webmvctest
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class WebmvctestApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
