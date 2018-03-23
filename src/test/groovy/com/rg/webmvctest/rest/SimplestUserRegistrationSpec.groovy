e35db9a27fdf4f456f575150771392dfe1a7997f
Rafal Glowinski <rafal.glowinski@allegro.pl>
2018-03-23 19:00:27 +0100

    Add WebMvcTest with Spock assertions

--- src/test/groovy/com/rg/webmvctest/rest/SimplestUserRegistrationSpec.groovy
+++ src/test/groovy/com/rg/webmvctest/rest/SimplestUserRegistrationSpec.groovy
@@ -1,11 +1,13 @@
 package com.rg.webmvctest.rest
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import com.rg.webmvctest.domain.RegistrationService
 import com.rg.webmvctest.domain.UserRegistration
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.boot.test.context.TestConfiguration
 import org.springframework.context.annotation.Bean
+import org.springframework.http.HttpStatus
 import org.springframework.test.web.servlet.MockMvc
 import spock.lang.Specification
 import spock.mock.DetachedMockFactory
@@ -24,6 +26,9 @@ class SimplestUserRegistrationSpec extends Specification {
 
     @Autowired
     RegistrationService registrationService
+
+    @Autowired
+    ObjectMapper objectMapper
 
     def "should pass user registration details to domain component and return 'created' status"() {
         given:
@@ -52,6 +57,39 @@ class SimplestUserRegistrationSpec extends Specification {
         results.andExpect(jsonPath("\$.email_address").value("john.wayne@gmail.com"))
         results.andExpect(jsonPath("\$.name").value("John"))
         results.andExpect(jsonPath("\$.last_name").value("Wayne"))
+    }
+
+    def "should pass user registration details to domain component and return 'created' status - spock assertions"() {
+        given:
+        Map request = [
+                email_address : 'john.wayne@gmail.com',
+                name          : 'John',
+                last_name     : 'Wayne'
+        ]
+
+        and:
+        registrationService.registerUser('john.wayne@gmail.com', 'John', 'Wayne') >> new UserRegistration(
+                'registration-id-1',
+                'john.wayne@gmail.com',
+                'John',
+                'Wayne'
+        )
+
+        when:
+        def response = mvc.perform(
+                post("/registrations").contentType(APPLICATION_JSON).content(toJson(request))
+        ).andReturn().response  // notice the extra call to: andReturn()
+
+        then:
+        response.status == HttpStatus.CREATED.value()
+
+        and:
+        with (objectMapper.readValue(response.contentAsString, Map)) {
+            it.registration_id == "registration-id-1"
+            it.email_address == "john.wayne@gmail.com"
+            it.name == "John"
+            it.last_name == "Wayne"
+        }
     }
 
     @TestConfiguration
